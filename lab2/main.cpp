cc48cf3bf0fc4d681cd907cff722d0da703f880f
Szymon <szymon.zajda@tlen.pl>
2021-04-15 00:02:05 +0200

    Added init instance

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -17,7 +17,7 @@ struct job {
 
 
 void print(const std::vector<job> &vec) {
-    std::cout << "\nj:\n";
+    std::cout << "\npi:\n";
     for (const auto &v : vec) {
         std::cout << v.n << " ";
     }
@@ -45,8 +45,6 @@ void print(const std::vector<int> &vec) {
     std::cout << "\n";
 }
 
-void Schrage(uint16_t K, uint8_t G[], uint8_t N[], uint8_t t){}
-
 int main() {
     int seed{};
     int size{};
@@ -63,15 +61,10 @@ int main() {
                       ++i;
                       job.n = i;
                   });
-    int a{};
+    int a{0};
     std::for_each(jobs.begin(), jobs.end(), [&](const auto &job) {
         a += job.p;
     });
-
-    std::for_each(jobs.begin(), jobs.end(),
-                  [&](auto &job) {
-                      job.q = rng.nextInt(1, 29);
-                  });
 
     std::for_each(jobs.begin(), jobs.end(),
                   [&](auto &job) {
@@ -80,49 +73,62 @@ int main() {
 
     std::for_each(jobs.begin(), jobs.end(),
                   [&](auto &job) {
-                      std::cout << job.p << " ";
-                  });
-    std::for_each(jobs.begin(), jobs.end(),
-                    [&](auto &job) {
-                    std::cout << job.q << " ";
+                      job.q = rng.nextInt(1, a);
                   });
 
-    std::sort(jobs.begin(), jobs.end(), [&](const auto &lhs, const auto &rhs) {
-        return lhs.r < rhs.r;
-    });
-    std::vector<int> s_time;
-    std::vector<int> c_time;
-    s_time.push_back(std::max(jobs[0].r, 0));
-    c_time.push_back(s_time[0] + jobs[0].p);
-    for (size_t i = 1; i < size; ++i) {
-        s_time.push_back(std::max(jobs[i].r, c_time[i - 1]));
-        c_time.push_back(s_time[i] + jobs[i].p);
-    }
-
-    /* Main goal */
+std::cout << "Permutacja naturalna " << std::endl;
+print(jobs);
+std::cout << "\n";
 
     /* Schrage algorythm */
     std::vector<job> N = jobs;
     std::vector<job> G;
+    std::vector<job> pi;
     int k{};
-    int t = (*std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) {
+    int t = (*std::min_element(N.begin(), N.end(), [&]( const auto &lhs, const auto &rhs) {
         return lhs.r < rhs.r;
     })).r;
 
-    while(G.empty() || N.empty()) {
-        while (N.size() && (*std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) {
-            return lhs.r < rhs.r;
-        })).r <= t){
-            auto it = (std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) {
+    while(G.size() || N.size()) {   /* while G != 0 or N != 0 */
+        while (N.size() &&
+               (*std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) { /* while N != 0 and min(r) <t */
+                   return lhs.r < rhs.r;
+               })).r <= t) {
+            auto it = (std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) { /* j <- argmin(rj) */
                 return lhs.r < rhs.r;
             }));
-        G.emplace_back(*it);
-        N.erase(it);
+            G.emplace_back(*it); /* G <- u {j*} */
+            N.erase(it);         /* N <- \{j*} */
+        }
+        if (G.size()) {
+
+            auto it = (std::max_element(G.begin(), G.end(), [&](const auto &lhs, const auto &rhs) {
+                return lhs.q < rhs.q;
+            }));
+            pi.push_back(*it);
+            t += it->p;
+            G.erase(it);
+            ++k;
+        } else {
+            t = (*std::min_element(N.begin(), N.end(), [&](const auto &lhs, const auto &rhs) {
+                return lhs.r < rhs.r;
+            })).r;
+        }
     }
+    std::vector<int> s_time;
+    std::vector<int> c_time;
+    int c_max{};
+    s_time.push_back(std::max(pi[0].r, 0));
+    c_time.push_back(s_time[0] + pi[0].p);
+    c_max = c_time[0] + pi[0].q;
+    for (size_t i = 1; i < size; ++i) {
+        s_time.push_back(std::max(pi[i].r, c_time[i - 1]));
+        c_time.push_back(s_time[i] + pi[i].p);
+        c_max = std::max(c_max,c_time[i]+pi[i].q);
     }
-
-
-
-    
-
+std::cout << "c_max\n";
+    std:: cout << c_max;
+    std:: cout << "\n";
+    std::cout << "Po algorytmie Shrage" << std::endl;
+    print(pi);
 }
